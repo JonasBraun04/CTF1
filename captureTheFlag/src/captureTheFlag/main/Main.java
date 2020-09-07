b35f731d11d0b748d68b2d17de47db8b9ff793f7
IsiTapier <41296026+IsiTapier@users.noreply.github.com>
2020-09-07 11:34:33 +0200

    c

--- captureTheFlag/src/captureTheFlag/main/Main.java
+++ captureTheFlag/src/captureTheFlag/main/Main.java
@@ -1,17 +1,25 @@
 package captureTheFlag.main;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		
+		loadConfig();
+		this.getConfig().set("Config.test.to.String", "HalloWelt!");
+		String test = this.getConfig().getString("Config.test");
+		Bukkit.broadcastMessage(test);
 	}
 	
 	@Override
 	public void onDisable() {
 		
 	}
-
+	
+	public void loadConfig() {
+		this.getConfig().options().copyDefaults(true);
+        saveConfig();
+	}
 }
